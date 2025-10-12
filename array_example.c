8e44c792340269da060549222b824f89f59aeeb4
Atik Hasan <atikhasan.io2042@gmail.com>
2025-10-12 23:17:44 +0600

    Add files via upload

---
+++ array_example.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int arr[7] = {10, 20, 30, 40, 50, 70, 80};
+
+    printf("Array elements are:\n");
+    for(int i = 0; i < 7; i++) {
+        printf("%d\n", arr[i]);
+    }
+
+    return 0;
+}
